ef7774ced926dc8405a332ad6da0d675b92baafa
jeremyaaronha <jeremyaaronha@gmail.com>
2025-05-13 03:59:50 -0400

    add

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const { initDb, getDatabase } = require('./data/database');
+
+const mongodb = require('./data/database');
 const app = express();
+
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => {
-  console.log(`Running on port ${port}`);
-});
+mongodb.initDb((err) => {
+        if (err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => {
+            console.log(`Running on port ${port}`);
+          });
+    }
+})
+
